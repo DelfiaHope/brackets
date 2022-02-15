4684267b07401d53eeee89546b50d8accf95d8dc
DelfiaHope <valeruanovna94@mail.ru>
2022-02-15 19:33:26 +0300

    add part of solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,73 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const OPEN_BRACKETS = ['(', '{' , '['];
+  const allBrackets = ['(', '{' , '[', ')', '}', ']']
+  const BRACKETS_PAIR = {
+    [')']: '(',
+    ['}']: '{',
+    [']']: '[',
+  }
+
+  function checkArray(bracketsConfig){
+ 
+    let stack = [];
+    for (let i = 0; i < bracketsConfig.length; i ++) {
+      let newStr = bracketsConfig[i].join('')
+      for (let j = 0; j < newStr.length; j ++) {
+        let currentSymbol = newStr[j];
+        console.log(currentSymbol)
+         if(allBrackets.includes(currentSymbol)){
+            if (OPEN_BRACKETS.includes(currentSymbol)) {
+              stack.push(currentSymbol);
+            } else {
+              if (stack.length === 0) {
+                return false;
+              } 
+      
+              let topElement = stack[stack.length - 1];
+      
+              if (BRACKETS_PAIR[currentSymbol] === topElement) {
+                stack.pop();
+              } else {
+                return false;
+              }
+             }
+      }else{ j++}
+      }
+    }
+    return stack.length === 0
+    
+  }
+   function checkStr(str){
+    let stack = [];
+       for (let j = 0; j < str.length; j ++) {
+            let currentSymbol = str[j];
+            console.log(currentSymbol)
+            if(allBrackets.includes(currentSymbol)){
+                if (OPEN_BRACKETS.includes(currentSymbol)) {
+                  stack.push(currentSymbol);
+                } else {
+                  if (stack.length === 0) {
+                    return false;
+                  } 
+          
+                  let topElement = stack[stack.length - 1];
+          
+                  if (BRACKETS_PAIR[currentSymbol] === topElement) {
+                    stack.pop();
+                  } else {
+                    return false;
+                  }
+                }
+            }else{ j++}
+       }
+       return stack.length === 0
+  }
+  
+  if (checkStr(str) && checkArray(bracketsConfig)){
+    return true
+  } else{
+    return false
+  } 
+  
 }
+
